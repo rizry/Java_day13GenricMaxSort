53a6c2ba42608f0538559e655c040dd4c9c5395c
rizry <rizzwan.ansarii@gmail.com>
2022-09-17 15:39:29 +0530

    added findMax for floats

--- MaxNum.java
+++ MaxNum.java
@@ -7,17 +7,17 @@ public class MaxNum {
     MaxNum maxNum = new MaxNum();
 
     //Test case 1: max number at 1st position
-    System.out.println("max= " + maxNum.findMax(10, 2, 3));
+    System.out.println("max= " + maxNum.findMax(10f, 2f, 3f));
 
     //Test case 1: max number at 2nd position
-    System.out.println("max= " + maxNum.findMax(10, 20, 3));
+    System.out.println("max= " + maxNum.findMax(10f, 20f, 3f));
 
     //Test case 1: max number at 3rd position
-    System.out.println("max= " + maxNum.findMax(10, 20, 30));
+    System.out.println("max= " + maxNum.findMax(10f, 20f, 30f));
   }
 
-  public int findMax(Integer a, Integer b, Integer c) {
-    Integer max = Integer.MIN_VALUE;
+  public float findMax(Float a, Float b, Float c) {
+    Float max = 0f;
 
     if (a.compareTo(b) > 0) {
       if (a.compareTo(c) > 0) max = a;
