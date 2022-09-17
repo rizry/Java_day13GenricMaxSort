a13d7edefa0bdaa494cc112dd4900231ec2f4d98
rizry <rizzwan.ansarii@gmail.com>
2022-09-17 17:43:18 +0530

    extended max method to sort based on option

--- MaxNum.java
+++ MaxNum.java
@@ -1,38 +1,47 @@
 package com.java.day13;
 
-public class MaxNum<E extends Comparable> {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FindMax<E extends Comparable> {
 
   private E a, b, c;
 
-  public MaxNum(E a, E b, E c) {
+  public FindMax(E a, E b, E c) {
     this.a = a;
     this.b = b;
     this.c = c;
-
-    this.testMaximum(); //constuctor calling the non static method testMaximum, which then calls static testMaximum method.
   }
 
-  public E testMaximum() {
-    E max = MaxNum.testMaximum(a, b, c);
-    System.out.println("max is " + max);
+  public E testMaximum(String option) {
+    E max = FindMax.testMaximum(a, b, c, option);
     return max;
   }
 
   public static void main(String[] args) {
-    //Test case 1: max number at 1st position
-    MaxNum<Integer> intIns = new MaxNum<>(10, 2, 3); //creating named-object for type Integer
+    //Test case 1: max int at 1st position
+    FindMax<Integer> intIns = new FindMax<>(10, 2, 3);
+    System.out.println("Max= " + intIns.testMaximum("desc") + "\n"); //calling the non-static method testMaximum which takes an option, and thats the fourth input for our static testMaximum method.
 
     //Test case 2: max float number at 2nd position
-    new MaxNum<Float>(10f, 20f, 3f); //creating anonymous object for type Float
+    FindMax<Float> floatIns = new FindMax<>(10f, 20f, 3f);
+    System.out.println("Max= " + floatIns.testMaximum("asc") + "\n");
 
     //Test case 3: max string at 3rd position
-    new MaxNum<String>("Chase", "Patric", "Ryan"); //creating anonymous object for type String
+    FindMax<String> stringIns = new FindMax<>("Patric", "Chase", "Ryan");
+    System.out.println("Max= " + stringIns.testMaximum("desc") + "\n");
 
   }
 
   @SuppressWarnings({"unchecked", "rawtypes"})
-  public static <E extends Comparable> E testMaximum(E a, E b, E c) {
+  public static <E extends Comparable> E testMaximum(E a, E b, E c, String option) {
     E max; //only declaring because we can't initialize value here without knowing its type
+
+    ArrayList<E> arrList = new ArrayList<>(); //using ArrayList instead of arrays because normal arrays need to be assigned memory beforehand and we don't know before method execution what data-type to store in them
+
+    arrList.add(a);
+    arrList.add(b);
+    arrList.add(c);
 
     if (a.compareTo(b) > 0) {
       if (a.compareTo(c) > 0) max = a;
@@ -40,6 +49,23 @@ public class MaxNum<E extends Comparable> {
 
     } else if (b.compareTo(c) > 0) max = b;
     else max = c;
+
+    switch (option) {
+    case "asc":
+      System.out.println("before sorting: " + arrList);
+      Collections.sort(arrList);
+      System.out.println("after sorting (asc): " + arrList);
+      break;
+
+    case "desc":
+      System.out.println("before sorting: " + arrList);
+      Collections.sort(arrList, Collections.reverseOrder());
+      System.out.println("after sorting (desc): " + arrList);
+      break;
+
+    default:
+      System.out.println("that option doesnt exist");
+    }
 
     return max;
   }
